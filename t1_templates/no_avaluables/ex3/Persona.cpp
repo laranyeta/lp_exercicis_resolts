6ee5319dd41032f7e67dc1db83e7b2bb2faac187
laranyeta <92856231+laranyeta@users.noreply.github.com>
2023-10-21 10:52:52 +0200

    Create Persona.cpp

---
+++ t1_templates/no_avaluables/ex3/Persona.cpp
@@ -0,0 +1,38 @@
+#include "Persona.h"
+
+ostream& operator<<(ostream &out, const Persona& p)
+{
+	out << "(" << p.m_nom << ", " << p.m_edat << ")";
+	return out;
+}
+
+bool Persona::operator>(const Persona& p)
+{
+	//mirem si una persona es mes gran que l'altra
+	bool esMajor = false;
+
+	if (m_edat > p.m_edat)
+		esMajor = true;
+	else if (m_edat == p.m_edat)
+	{
+		if (m_nom > p.m_nom)
+			esMajor = true;
+	}
+	return esMajor;
+}
+
+bool Persona::operator<(const Persona& p)
+{
+	//mirem si una persona es mes petita que l'altra
+	bool esMenor = false;
+
+	if (m_edat < p.m_edat)
+		esMenor = true;
+	else if (m_edat == p.m_edat)
+	{
+		if (m_nom < p.m_nom)
+			esMenor = true;
+	}
+	return esMenor;
+}
+
